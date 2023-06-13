4f359de7906ed87f4a1794487ec8cb871fbd08fa
Francois-s <119123991+Francois-s@users.noreply.github.com>
2023-06-13 20:07:17 +0200

    first push

---
+++ list.py
@@ -0,0 +1,19 @@
+import os
+
+def generate_image_tags(directory):
+    image_tags = ''
+    inc = 0
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            file_name, file_ext = os.path.splitext(filename)
+            source = "../shooting/" + directory_name + "/" + filename
+            image_tags += f'<img src="{source}" alt="{file_name}" class="card" id="{inc}_{directory_name}">\n'
+            inc = inc + 1
+    return image_tags
+
+# Remplacez le chemin du dossier ci-dessous par le dossier souhaité
+directory_path = 'shooting/philomene'
+directory_name = 'philomene'  # Remplacez par le nom du dossier
+
+image_tags = generate_image_tags(directory_path)
+print(image_tags)
